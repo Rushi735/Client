7606df381114ddb9c0ee7fbe4716af911fcc19cb
Rushikesh Baban Pawar <pawarrushikesh735@gmail.com>
2025-07-16 11:19:49 +0530

    Update user-dashboard.js

--- js/user-dashboard.js
+++ js/user-dashboard.js
@@ -582,7 +582,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         currentDriverId = driverId;
         updateDriverLocation(driverId); // Initial update
-        mapRefreshInterval = setInterval(() => updateDriverLocation(driverId), 30000); // Update every 10 seconds
+        mapRefreshInterval = setInterval(() => updateDriverLocation(driverId), 45000); // Update every 10 seconds
     }
 
     function startWatchingUserLocation() {
